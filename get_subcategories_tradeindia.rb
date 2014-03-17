c647046bfa2763d0847ce47d752867a0345f97bb
Paras <narangparas@gmail.com>
2014-03-17 21:26:53 +0530

    get tradeindia sub categories

--- get_subcategories_tradeindia.rb
+++ get_subcategories_tradeindia.rb
@@ -1,17 +1,18 @@
 require 'nokogiri'
 require 'open-uri'
 
-categories = File.open('tradeindia-category-links.txt').readlines
-filename = 'tradeindia-sub-category-links.txt'
+website = 'http://www.tradeindia.com'
+categories = File.open('data-tradeindia-category-links.txt').readlines
+filename = 'data-tradeindia-sub-category-links.txt'
 
 File.open(filename, 'a') do |f|
   categories.each do |category|
     doc = Nokogiri::HTML(open(category))
 
     doc.css("a[@class='categorylist']").each do |subcategory|
-      content = subcategory.href
+      content = website + subcategory[:href]
       f.puts content
-      puts "#{subcategory} DONE"
+      puts "#{subcategory[:href]} DONE"
     end
     puts "#{category} DONE"
   end
