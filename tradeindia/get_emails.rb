e14066589f2f142985f4d04905807fd96d3b85f5
Inderpal Singh <inderps@thoughtworks.com>
2014-04-15 00:32:23 +0530

    added error handling

--- tradeindia/get_emails.rb
+++ tradeindia/get_emails.rb
@@ -7,30 +7,31 @@ regex = Regexp.new('/[\w.!#\$%+-]+@[\w-]+(?:\.[\w-]+)+/')
 websites.each do |website|
   emails = []
 
-  doc = Nokogiri::HTML(open(website.strip))
-  puts '-- Extracting from ' + website.strip
+  begin
+    doc = Nokogiri::HTML(open(website.strip))
+    puts '-- Extracting from ' + website.strip
 
-  # Searching by css
-  doc.css('.email_field').each do |email|
-    emails.push(email) unless email.include?('tradeindia')
-  end
+    # Searching by css
+    doc.css('.email_field').each do |email|
+      emails.push(email) unless email.include?('tradeindia')
+    end
 
-  # Searching by mailto links
-  selector = "//a[starts-with(@href, \"mailto:\")]/@href"
-  nodes = doc.xpath selector
-  addresses = nodes.collect {|n| n.value[7..-1]}
-  #Since mail-to can have multiple addresses separated by commas
-  addresses.each do |address|
-    address.split(',').each do |addr|
-      emails << addr unless addr.include?('tradeindia')
+    # Searching by mailto links
+    selector = "//a[starts-with(@href, \"mailto:\")]/@href"
+    nodes = doc.xpath selector
+    addresses = nodes.collect {|n| n.value[7..-1]}
+    #Since mail-to can have multiple addresses separated by commas
+    addresses.each do |address|
+      address.split(',').each do |addr|
+        emails << addr unless addr.include?('tradeindia')
+      end
+    end
+
+    File.open('data-vendor-emails.txt', 'a') do |f|
+      f.puts emails.uniq
+      puts emails.uniq
     end
   end
-
-  File.open('data-vendor-emails.txt', 'a') do |f|
-    f.puts emails.uniq
-    puts emails.uniq
-  end
-
 end
 
 
